65274bbc5a3c0a22c89e739247a4bc82daec9d75
Moncef <mf.aoudia@gmail.com>
2020-11-01 23:46:01 +0100

    refactor: skip running validation on null value
    resolve #29

--- src/test/groovy/graphql/validation/constraints/standard/NoEmptyBlankConstraintTest.groovy
+++ src/test/groovy/graphql/validation/constraints/standard/NoEmptyBlankConstraintTest.groovy
@@ -24,13 +24,13 @@ class NoEmptyBlankConstraintTest extends BaseConstraintTestSupport {
         'field( arg : String @NotBlank ) : ID' | "\t\n\r "  | 'NotBlank;path=/arg;val:\t\n\r ;\t'
         'field( arg : String @NotBlank ) : ID' | ""         | 'NotBlank;path=/arg;val:;\t'
         'field( arg : String @NotBlank ) : ID' | "\t\n\r X" | ''
-        'field( arg : String @NotBlank ) : ID' | null       | 'NotBlank;path=/arg;val:null;\t'
+        'field( arg : String @NotBlank ) : ID' | null       | ''
 
         // IDs
         'field( arg : ID @NotBlank ) : ID' | "\t\n\r "  | 'NotBlank;path=/arg;val:\t\n\r ;\t'
         'field( arg : ID @NotBlank ) : ID' | ""         | 'NotBlank;path=/arg;val:;\t'
         'field( arg : ID @NotBlank ) : ID' | "\t\n\r X" | ''
-        'field( arg : ID @NotBlank ) : ID' | null       | 'NotBlank;path=/arg;val:null;\t'
+        'field( arg : ID @NotBlank ) : ID' | null       | ''
     }
 
     @Unroll
@@ -48,28 +48,28 @@ class NoEmptyBlankConstraintTest extends BaseConstraintTestSupport {
         fieldDeclaration                            | argVal      | expectedMessage
         // strings
         'field( arg : String @NotEmpty ) : ID'      | ""          | 'NotEmpty;path=/arg;val:;\t'
-        'field( arg : String @NotEmpty ) : ID'      | null        | 'NotEmpty;path=/arg;val:null;\t'
+        'field( arg : String @NotEmpty ) : ID'      | null        | ''
         'field( arg : String @NotEmpty ) : ID'      | "\t\n\r"    | ''
         'field( arg : String @NotEmpty ) : ID'      | "ABC"       | ''
 
         // IDs
         'field( arg : ID @NotEmpty ) : ID'      | ""          | 'NotEmpty;path=/arg;val:;\t'
-        'field( arg : ID @NotEmpty ) : ID'      | null        | 'NotEmpty;path=/arg;val:null;\t'
+        'field( arg : ID @NotEmpty ) : ID'      | null        | ''
         'field( arg : ID @NotEmpty ) : ID'      | "\t\n\r"    | ''
         'field( arg : ID @NotEmpty ) : ID'      | "ABC"       | ''
 
 
         // objects
         'field( arg : InputObject @NotEmpty ) : ID' | [:]         | 'NotEmpty;path=/arg;val:[:];\t'
-        'field( arg : InputObject @NotEmpty ) : ID' | null        | 'NotEmpty;path=/arg;val:null;\t'
+        'field( arg : InputObject @NotEmpty ) : ID' | null        | ''
         'field( arg : InputObject @NotEmpty ) : ID' | [name: "x"] | ''
 
         // lists
         'field( arg : [String] @NotEmpty ) : ID'    | []          | 'NotEmpty;path=/arg;val:[];\t'
-        'field( arg : [String] @NotEmpty ) : ID'    | null        | 'NotEmpty;path=/arg;val:null;\t'
+        'field( arg : [String] @NotEmpty ) : ID'    | null        | ''
         'field( arg : [String] @NotEmpty ) : ID'    | ["x"]       | ''
         'field( arg : [ID] @NotEmpty ) : ID'    | []          | 'NotEmpty;path=/arg;val:[];\t'
-        'field( arg : [ID] @NotEmpty ) : ID'    | null        | 'NotEmpty;path=/arg;val:null;\t'
+        'field( arg : [ID] @NotEmpty ) : ID'    | null        | ''
         'field( arg : [ID] @NotEmpty ) : ID'    | ["x"]       | ''
 
     }
