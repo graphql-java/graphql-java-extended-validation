01edca814c76681c7100e42671ce9eb1997d4592
sunny pelletier <sunny.pelletier@qohash.com>
2021-06-01 11:00:42 -0400

    refix imports

--- src/main/java/graphql/validation/interpolation/ResourceBundleMessageInterpolator.java
+++ src/main/java/graphql/validation/interpolation/ResourceBundleMessageInterpolator.java
@@ -29,7 +29,12 @@ import java.util.MissingResourceException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
