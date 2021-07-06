2e912e84c7739e9e774b88ad46e38465858e8a8c
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-07-06 14:25:08 -0400

    remove useless dupplication of APIs

--- src/main/java/graphql/validation/interpolation/ResourceBundleMessageInterpolator.java
+++ src/main/java/graphql/validation/interpolation/ResourceBundleMessageInterpolator.java
@@ -22,19 +22,9 @@ import javax.validation.Path;
 import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.util.LinkedHashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
