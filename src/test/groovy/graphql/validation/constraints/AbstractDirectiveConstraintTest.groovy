50424e8bdcd5bfbdab993cea502b8ea8d04f5776
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-07-25 16:09:02 -0400

    update

--- src/test/groovy/graphql/validation/constraints/AbstractDirectiveConstraintTest.groovy
+++ src/test/groovy/graphql/validation/constraints/AbstractDirectiveConstraintTest.groovy
@@ -1,6 +1,6 @@
 package graphql.validation.constraints
 
-import graphql.validation.constraints.standard.ContainerSizeConstraint
+
 import graphql.validation.constraints.standard.SizeConstraint
 import spock.lang.Unroll
 
@@ -8,11 +8,8 @@ class AbstractDirectiveConstraintTest extends BaseConstraintTestSupport {
 
     @Unroll
     def "complex object argument constraints"() {
-        def sizeConstraint = new SizeConstraint()
 
         def extraSDL = """
-            ${sizeConstraint.getDocumentation().getDirectiveSDL()}
-
             input ProductItem {
                 code : String @Size(max : 5)
                 price : String @Size(max : 3)
@@ -27,7 +24,7 @@ class AbstractDirectiveConstraintTest extends BaseConstraintTestSupport {
 
         // this tests that we can walk a complex tree of types via one specific implementation
         // but the same applies to all AbstractDirectiveConstraint classes
-        def constraintUnderTest = new ContainerSizeConstraint()
+        def constraintUnderTest = new SizeConstraint()
 
         expect:
 
@@ -36,18 +33,18 @@ class AbstractDirectiveConstraintTest extends BaseConstraintTestSupport {
 
         where:
 
-        fieldDeclaration                                             | argVal                                               | eSize | expectedMessage
+        fieldDeclaration                     | argVal                                               | eSize | expectedMessage
         // size can handle list elements
-        "field( testArg : [Product!] @ContainerSize(max : 2) ) : ID" | [[:], [:], [:]]                                      | 1     | "graphql.validation.Size.message;path=/testArg;val:[[:], [:], [:]];\t"
+        "field( testArg : [Product!] ) : ID" | [[:], [:], [:]]                                      | 0     | ""
         // goes down into input types
-        "field( testArg : [Product!] @ContainerSize(max : 2) ) : ID" | [[name: "morethan7"], [:]]                           | 1     | "graphql.validation.Size.message;path=/testArg[0]/name;val:morethan7;\t"
+        "field( testArg : [Product!] ) : ID" | [[name: "morethan7"], [:]]                           | 1     | "graphql.validation.Size.message;path=/testArg[0]/name;val:morethan7;\t"
         // shows that it traverses down lists
-        "field( testArg : [Product!] @ContainerSize(max : 2) ) : ID" | [[name: "ok"], [name: "notOkHere"]]                  | 1     | "graphql.validation.Size.message;path=/testArg[1]/name;val:notOkHere;\t"
+        "field( testArg : [Product!] ) : ID" | [[name: "ok"], [name: "notOkHere"]]                  | 1     | "graphql.validation.Size.message;path=/testArg[1]/name;val:notOkHere;\t"
         // shows that it traverses down lists and objects
-        "field( testArg : [Product!] @ContainerSize(max : 2) ) : ID" | [[items: [[code: "morethan5", price: "morethan3"]]]] | 2     | "graphql.validation.Size.message;path=/testArg[0]/items[0]/code;val:morethan5;\tgraphql.validation.Size.message;path=/testArg[0]/items[0]/price;val:morethan3;\t"
+        "field( testArg : [Product!] ) : ID" | [[items: [[code: "morethan5", price: "morethan3"]]]] | 2     | "graphql.validation.Size.message;path=/testArg[0]/items[0]/code;val:morethan5;\tgraphql.validation.Size.message;path=/testArg[0]/items[0]/price;val:morethan3;\t"
 
         // shows that it traverses down crazy lists and objects
-        "field( testArg : [Product!] @ContainerSize(max : 2) ) : ID" | [[crazyItems: [[[[code: "morethan5"]]]]]]            | 1     | "graphql.validation.Size.message;path=/testArg[0]/crazyItems[0][0][0]/code;val:morethan5;\t"
+        "field( testArg : [Product!] ) : ID" | [[crazyItems: [[[[code: "morethan5"]]]]]]            | 1     | "graphql.validation.Size.message;path=/testArg[0]/crazyItems[0][0][0]/code;val:morethan5;\t"
     }
 
 }
